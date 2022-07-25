398027f17864921376a3b87b6f1425dcd865d4e3
ramesh25022001 <61628444+ramesh25022001@users.noreply.github.com>
2022-07-25 17:10:24 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -71,7 +71,7 @@ async def private_receive_handler(c: Client, m: Message):
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         file_name = get_media_file_name(m)
         file_size = humanbytes(get_media_file_size(m))
-        stream_link = "https://{}/{}/{}".format(Var.FQDN, log_msg.id, file_name) if Var.ON_HEROKU or Var.NO_PORT else \
+        stream_link = "https://stream.url2go.in//st?api=af5e38dfaf8b900b45335173d279b44d7ae4b2e9&url=https://{}/{}/{}".format(Var.FQDN, log_msg.id, file_name) if Var.ON_HEROKU or Var.NO_PORT else \
             "http://{}:{}/{}/{}".format(Var.FQDN,
                                     Var.PORT,
                                     log_msg.id,
