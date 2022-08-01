675f016294b1a9a731c128d85795c62700e6c8a8
ramesh25022001 <61628444+ramesh25022001@users.noreply.github.com>
2022-08-01 23:34:25 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -118,8 +118,8 @@ async def channel_receive_handler(bot, broadcast):
             chat_id=broadcast.chat.id,
             message_id=broadcast.id,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ 📥", url=f"https://t.me/{(await bot.get_me()).username}?start=AvishkarPatil_{str(log_msg.id)}")]])
-            # [[InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ 📥", url=f"https://t.me/FxStreamBot?start=AvishkarPatil_{str(log_msg.id)}")]])
+                [[InlineKeyboardButton("👀 Watch / Direct Download 📥", url=f"https://t.me/{(await bot.get_me()).username}?start=A_{str(log_msg.id)}")]])
+            # [[InlineKeyboardButton("👀 Watch / Direct Download 📥", url=f"https://t.me/FxStreamBot?start=AvishkarPatil_{str(log_msg.id)}")]])
         )
     except FloodWait as w:
         print(f"Sleeping for {str(w.x)}s")
